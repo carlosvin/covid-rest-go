c8b036006b129934fc1c2fc5797cb859dbb8a0f6
Carlos Martin Sanchez <carlosvin@gmail.com>
2020-09-27 20:56:27 +0200

    Returning directoly dictionaries, there is no need to use gin.H

--- handlers/countries.go
+++ handlers/countries.go
@@ -25,14 +25,14 @@ func NewRouter(repo repo.Repo) Router {
 }
 
 func (r *routerImpl) Countries(c *gin.Context) {
-	c.JSON(200, gin.H{"countries": r.countries()})
+	c.JSON(200, r.countries())
 }
 
 func (r *routerImpl) Country(c *gin.Context) {
 	code := strings.ToUpper(c.Param("code"))
 	resp := r.country(code)
 	if resp != nil {
-		c.JSON(200, gin.H{"country": resp})
+		c.JSON(200, resp)
 	} else {
 		c.JSON(404, gin.H{"message": "not found"})
 	}
@@ -42,7 +42,7 @@ func (r *routerImpl) CountryDates(c *gin.Context) {
 	code := strings.ToUpper(c.Param("code"))
 	resp := r.countryDates(code)
 	if resp != nil {
-		c.JSON(200, gin.H{"dates": resp})
+		c.JSON(200, resp)
 	} else {
 		c.JSON(404, gin.H{"message": "not found"})
 	}
@@ -53,7 +53,7 @@ func (r *routerImpl) CountryDate(c *gin.Context) {
 	dateParam := c.Param("date")
 	resp := r.countryDate(codeParam, dateParam)
 	if resp != nil {
-		c.JSON(200, gin.H{"date": r.countryDate(codeParam, dateParam)})
+		c.JSON(200, r.countryDate(codeParam, dateParam))
 	} else {
 		c.JSON(404, gin.H{"message": "not found"})
 	}
