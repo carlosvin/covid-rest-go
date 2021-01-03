0ad69604c0f8b0539caa07bc2baf7e6baafe41e9
Carlos Martin Sanchez <carlmartin@ebay.com>
2021-01-03 20:59:07 +0100

    trying to use swagger and openapi autogen

--- go.mod
+++ go.mod
@@ -2,4 +2,22 @@ module github.com/carlosvin/covid-rest-go
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.6.3
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/gin-gonic/gin v1.6.3
+	github.com/go-openapi/spec v0.20.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
+	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.7.0
+	github.com/ugorji/go v1.2.2 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	golang.org/x/sys v0.0.0-20201231184435-2d18734c6014 // indirect
+	golang.org/x/tools v0.0.0-20210102185154-773b96fafca2 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -8,9 +8,25 @@ import (
 	"github.com/carlosvin/covid-rest-go/handlers"
 	"github.com/carlosvin/covid-rest-go/readers"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
+	_ "github.com/swaggo/gin-swagger/example/basic/docs"
 )
 
+// @title COVID cases tracking API
+// @version 1.0
+// @description Exposes COVID-19 statistics extracted from daily published data by European Centre for Disease Prevention and Control at: https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide.
+// @contact.name API support
+// @contact.url https://github.com/carlosvin/covid-rest-go/issues
+// @license.name MIT
+// @license.url ./LICENSE
+
+// @host covid-rest.appspot.com
+// @BasePath /
 func main() {
+	url := ginSwagger.URL("http://localhost:8080/docs/doc.json") // The url pointing to API definition
+
 	repository := repo.NewRepo(readers.NewReaderFactory())
 	router := handlers.NewRouter(repository)
 	err := repository.Fetch()
@@ -23,5 +39,7 @@ func main() {
 	r.GET("/countries/:code", router.Country)
 	r.GET("/countries/:code/dates", router.CountryDates)
 	r.GET("/countries/:code/dates/:date", router.CountryDate)
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
+
 	r.Run()
 }
